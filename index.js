0a6053a9c25bd2f623b7c102076ea905fe451ac6
Matheus Macabu <macabu.matheus@gmail.com>
2019-03-26 14:00:15 +0100

    chore: initial version

---
+++ index.js
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: ['airbnb-base'],
+  rules: {
+    semi: ['error', 'never'],
+    'comma-dangle': ['error', {
+      arrays: 'always-multiline',
+      objects: 'always-multiline',
+      imports: 'always-multiline',
+      exports: 'always-multiline',
+      functions: 'never',
+    }],
+    'function-paren-newline': ['error', 'multiline'],
+    'max-len': ['error', 80, 2, {
+      ignoreUrls: true,
+      ignoreComments: false,
+      ignoreTrailingComments: true,
+      ignoreRegExpLiterals: true,
+      ignoreStrings: true,
+      ignoreTemplateLiterals: true,
+    }],
+  },
+}
